762752039e5ffd11622028ed10f9c3e0ac61e295
SergeyKov <skov82@ya.ru>
2022-09-06 23:56:47 +0300

    add dist folder

---
+++ dist/index.7a9f23bf.js
@@ -0,0 +1,2 @@
+document.querySelector("#root").textContent=function(...e){if(!e.length)throw Error("sum required at least 1 argument");return e.reduce(((e,t)=>e+t),0)}(3,2);
+//# sourceMappingURL=index.7a9f23bf.js.map
